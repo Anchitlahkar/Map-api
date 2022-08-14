55e81baf3133ec580fefc7127872a79734a5ace3
Anchit <anchitlahkar0202@gmail.com>
2022-08-14 11:21:13 +0530

    14.08.2022

--- server.js
+++ server.js
@@ -2,21 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const api = express();
 api.set("view engine", "ejs");
-const server = require("http").Server(api);
 
 let route
-
-const io = require("socket.io")(server, {
-    cors: {
-        origin: '*'
-    }
-});
 
 
 api.use(express.static('public'))
 api.use(bodyParser.json())
 
-api.listen(process.env.PORT, () => {
+api.listen(process.env.PORT || 3000, () => {
     console.log('Api Running')
 })
 
@@ -32,5 +25,3 @@ api.post('/location-details', (req, res) => {
     console.log(req.body)
     route = req.body
 })
-
-server.listen(process.env.PORT || 3000);
